435193fc157f87ee6a10628ea0526248a7606bd9
Sarthaksingh2005 <sarthakash26@gmail.com>
2025-11-11 22:46:15 +0530

    Add getIOS and deleteNode functions for binary tree
    Implement getIOS function to find the in-order successor in a binary tree and add deleteNode function to remove a node from the binary search tree.

---
+++ getIOS.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode*getIOS(TreeNode*node)
+    {
+        TreeNode*curr=node;
+        while(curr!=NULL&&curr->left!=NULL)
+        {
+            curr=curr->left;
+        }
+        return curr;
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) 
+    {
+        if(root==NULL)
+        {
+            return NULL;
+        }
+        if(key<root->val)
+        {root->left=deleteNode(root->left,key);}
+        else if(key>root->val)
+        {root->right=deleteNode(root->right,key);}
+        else
+        {
+            if(root->left==NULL)
+            {
+                TreeNode*temp=root->right;
+                delete(root);
+                return temp;
+            }
+            else if(root->right== NULL)
+            {
+            TreeNode*temp=root->left;
+                delete(root);
+                return temp;
+            }
+            else
+            {
+                TreeNode*IOS=getIOS(root->right);
+                root->val=IOS->val;
+                root->right=deleteNode(root->right,IOS->val);
+            }
+        }
+        return root;
+    }
+};
